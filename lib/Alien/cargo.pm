4ff524b2a58211fe5a4ad369dd86478c60481493
Graham Ollis <plicease@cpan.org>
2024-11-23 14:48:16 -0700

    Initial structure

---
+++ lib/Alien/cargo.pm
@@ -0,0 +1,13 @@
+package Alien::cargo;
+
+use strict;
+use warnings;
+use 5.008004;
+use base qw( Alien::Base );
+
+# ABSTRACT: Find or download the cargo command (build system and package manager for Rust)
+# VERSION
+
+1;
+
+
